d028efdc7c498abb4e42583158acd91d9ea7fb66
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2023-11-13 20:54:35 +0300

    rearrange category

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,39 +1,35 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
-export default class CategoryList extends Component {
-  state = {
-    categories: []
-  };
-  componentDidMount() {
-    this.getCategories();
-  }
 
-  getCategories = () => {
-    fetch("http://localhost:3000/categories")
-      .then(response => response.json())
-      .then(data =>
-        this.setState({
-          categories: data
-        })
-      );
-  };
-  render() {
-    return (
-      <div>
-        <h2>{this.props.info.title}</h2>
-        <ListGroup>
-          {this.state.categories.map(category => (
-            <ListGroupItem
-              onClick={() => this.props.chanceCategory(category)}
-              key={category.id}
-              active={category.categoryName === this.props.currentCategory}
-            >
-              {category.categoryName}
-            </ListGroupItem>
-          ))}
-        </ListGroup>
-        {/* <h2>{this.props.currentCategory} </h2> */}
-      </div>
-    );
-  }
-}
+const CategoryList = (props) => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = () => {
+      fetch("http://localhost:3000/categories")
+        .then((response) => response.json())
+        .then((data) => setCategories(data));
+    };
+
+    getCategories();
+  }, []);
+
+  return (
+    <div>
+      <h2>{props.info.title}</h2>
+      <ListGroup>
+        {categories.map((category) => (
+          <ListGroupItem
+            onClick={() => props.chanceCategory(category)}
+            key={category.id}
+            active={category.categoryName === props.currentCategory}
+          >
+            {category.categoryName}
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+    </div>
+  );
+};
+
+export default CategoryList;
